783c9e8fdfc49eb4134f7b988bc6fa63d78e695c
blumry <firemoth14@gmail.com>
2024-03-06 20:03:32 +0900

    trie

--- lecture_코딩테스트/7.실습.js
+++ lecture_코딩테스트/7.실습.js
@@ -173,3 +173,45 @@ for (let i = 0; i < 5; i++) {
   heap.push(Math.floor(Math.random() * 100));
 }
 console.log(heap.heap);
+
+class Node {
+  constructor(value = "") {
+    this.value = value;
+    this.children = new Map();
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+
+  insert(string) {
+    let currentNode = this.root;
+
+    for (const char of string) {
+      if (!currentNode.children.has(char)) {
+        currentNode.children.set(char, new Node(currentNode.value + char));
+      }
+    }
+    currentNode = currentNode.children.get(char);
+  }
+
+  has(string) {
+    let currentNode = this.root;
+
+    for (const char of string) {
+      if (!currentNode.childNodes.has(char)) {
+        return false;
+      }
+      currentNode = currentNode.children.get(char);
+    }
+    return true;
+  }
+}
+
+const trie = new Trie();
+trie.insert("apple");
+trie.insert("banana");
+console.log(trie.has("apple"));
+console.log(trie.has("app"));
