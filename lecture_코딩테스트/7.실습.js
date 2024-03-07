26b8385eee8f844d15188f1c60ca8eddb52faa7e
blumry <firemoth14@gmail.com>
2024-03-07 22:53:51 +0900

    review_stack,linkedlist

--- lecture_코딩테스트/7.실습.js
+++ lecture_코딩테스트/7.실습.js
@@ -5,213 +5,163 @@
 // find, append, remove, insert, display
 // 1. singly
 
-// class Node {
-//   constructor(value) {
-//     this.value = value;
-//     this.next = null;
-//   }
-// }
-// class SinglyLinkedList {
-//   constructor() {
-//     this.head = null;
-//     this.tail = null;
-//   }
-
-//   find(value) {
-//     let currNode = this.head;
-//     while (currNode.value !== value) {
-//       currNode = currNode.next;
-//     }
-//     console.log(currNode);
-//     return currNode;
-//   }
-
-//   append(value) {
-//     let newNode = new Node(value);
-//     if (this.head) {
-//       newNode.next = this.head;
-//       this.head = newNode;
-//     } else {
-//       this.head = newNode;
-//     }
-//   }
-
-//   insert(prevValue, value) {
-//     let newNode = new Node(value);
-//     let prevNode = this.find(prevValue);
-//     newNode.next = prevNode.next;
-//     prevNode.next = newNode;
-//   }
-
-//   remove(value) {
-//     let removeNode = this.find(value);
-//     let prevNode = this.head;
-//     while (prevNode.next.value !== value) {
-//       prevNode = prevNode.next;
-//     }
-//     prevNode.next = removeNode.next;
-//   }
-
-//   display() {
-//     let result = [];
-//     let currNode = this.head;
-//     while (currNode !== null) {
-//       result.push(currNode.value);
-//       currNode = currNode.next;
-//     }
-//     console.log(result);
-//     return result;
-//   }
-// }
-// const test = new SinglyLinkedList();
-// test.append(1);
-// test.append(4);
-// test.append(6);
-// test.append(10);
-// test.display();
-// test.find(1);
-// test.remove(6);
-// test.display();
-// test.insert(4, 15);
-// test.display();
-
-// 2. stack;
-
-// Node
-// - value / pointer -> top / v
-// - 추가 : "맨위"로
-// class Node {
-//   constructor(value) {
-//     this.value = value;
-//     this.next = null;
-//   }
-// }
-
-// class Stack {
-//   constructor() {
-//     this.top = null;
-//     this.size = 0;
-//   }
-
-//   push(value) {
-//     const node = new Node(value);
-//     node.next = this.top;
-//     this.top = node;
-//     this.size += 1;
-//   }
-
-//   pop() {
-//     const value = this.top.value;
-//     this.top = this.top.next;
-//     this.size -= 1;
-//     return value;
-//   }
-
-//   size() {
-//     return this.size;
-//   }
-// }
-
-class Queue {
+/* 연결리스트
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class SinglyLinkedList {
   constructor() {
-    this.queue = [];
-    this.front = 0;
-    this.rear = 0;
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(value) {
-    this.queue[this.rear++] = value;
+  find(value) {
+    let currNode = this.head;
+    while (currNode.value !== value) {
+      currNode = currNode.next;
+    }
+    console.log(currNode);
+    return currNode;
   }
 
-  dequeue(value) {
-    this.value = this.queue[this.front];
-    delete this.queue[this.front];
-    this.front += 1;
-    return value;
+  append(value) {
+    let newNode = new Node(value);
+    if (this.head) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      this.head = newNode;
+    }
   }
 
-  peek() {
-    return this.queue[this.front];
+  insert(prevValue, value) {
+    let newNode = new Node(value);
+    let prevNode = this.find(prevValue);
+    newNode.next = prevNode.next;
+    prevNode.next = newNode;
   }
 
-  size() {
-    return this.rear - this.front;
+  remove(value) {
+    let removeNode = this.find(value);
+    let prevNode = this.head;
+    while (prevNode.next.value !== value) {
+      prevNode = prevNode.next;
+    }
+    prevNode.next = removeNode.next;
+  }
+
+  display() {
+    let result = [];
+    let currNode = this.head;
+    while (currNode !== null) {
+      result.push(currNode.value);
+      currNode = currNode.next;
+    }
+    console.log(result);
+    return result;
+  }
+}
+const test = new SinglyLinkedList();
+test.append(1);
+test.append(4);
+test.append(6);
+test.append(10);
+test.display();
+test.find(1);
+test.remove(6);
+test.display();
+test.insert(4, 15);
+test.display();
+*/
+
+/*스택
+
+Node
+- value / pointer -> top / v
+- 추가 : "맨위"로
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
-const queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-console.log(queue.dequeue());
-console.log(queue.size());
-
-class MaxHeap {
+class Stack {
   constructor() {
-    this.heap = [];
+    this.top = null;
+    this.size = 0;
   }
 
   push(value) {
-    this.heap.push(value);
-    let currentIndex = this.heap.length - 1;
-    let parentIndex = Math.floor((currentIndex - 1) / 2);
+    const node = new Node(value);
+    node.next = this.top;
+    this.top = node;
+    this.size += 1;
+  }
 
-    while (parentIndex !== 0 && this.heap[parentIndex] < value) {
-      const temp = this.heap[parentIndex];
-      this.heap[parentIndex] = value;
-      this.heap[currentIndex] = temp;
+  pop() {
+    const value = this.top.value;
+    this.top = this.top.next;
+    this.size -= 1;
+    return value;
+  }
 
-      currentIndex = parentIndex;
-      parentIndex = Math.floor((currentIndex - 1) / 2);
-    }
+  size() {
+    return this.size;
   }
 }
+*/
 
-const heap = new MaxHeap();
+/* 스택 문제 : 올바른 괄호
+ - 핵심은 문자열 맨 처음과 끝
+  - 그냥 유사배열로서 맨처음 인덱스랑 맨끝 인덱스만 검사하면 되는거 아닌가..
+  - 어떤 부분에서 스택을 적용하는건지..
+*/
 
-for (let i = 0; i < 5; i++) {
-  heap.push(Math.floor(Math.random() * 100));
+function solution(string) {
+  const start = new Date().getTime();
+  let result = string[0] === "(" && string[string.length - 1] === ")";
+  const end = new Date().getTime();
+  console.log("sol end" + String(end - start));
+  return result;
 }
-console.log(heap.heap);
 
-class Node {
-  constructor(value = "") {
-    this.value = value;
-    this.children = new Map();
-  }
-}
+function solution2(s) {
+  const start = new Date().getTime();
+  let count = 0;
 
-class Trie {
-  constructor() {
-    this.root = new Node();
-  }
-
-  insert(string) {
-    let currentNode = this.root;
-
-    for (const char of string) {
-      if (!currentNode.children.has(char)) {
-        currentNode.children.set(char, new Node(currentNode.value + char));
-      }
-    }
-    currentNode = currentNode.children.get(char);
-  }
-
-  has(string) {
-    let currentNode = this.root;
-
-    for (const char of string) {
-      if (!currentNode.childNodes.has(char)) {
+  for (const c of s) {
+    if (c === "(") {
+      count += 1;
+    } else {
+      if (count === 0) {
+        const end = new Date().getTime();
+        console.log(`sol2 end : ${end - start}`);
         return false;
       }
-      currentNode = currentNode.children.get(char);
+      count -= 1;
     }
-    return true;
   }
+  const end = new Date().getTime();
+  console.log(`sol2 end : ${end - start}`);
+  return count === 0;
 }
 
-const trie = new Trie();
-trie.insert("apple");
-trie.insert("banana");
-console.log(trie.has("apple"));
-console.log(trie.has("app"));
+function testCreate() {
+  let result = "";
+
+  for (let i = 0; i < 10000000; i++) {
+    Math.floor(Math.random() * 2) ? (result += ")") : (result += "(");
+  }
+
+  return String(result);
+}
+
+let test = testCreate();
+solution2(test);
+console.log(performance.now(solution2));
+console.log(performance.now(solution));
+solution(test);
